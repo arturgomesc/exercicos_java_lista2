a4c0ec552e6929a97e2ecb1d9cc2cfde1997cdb1
arturgomesc <arturgomesc@hotmail.com>
2023-05-05 10:24:32 -0300

    iniciando a questao 3 (estudar arrays)

---
+++ src/quatroValores.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class quatroValores {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+
+        System.out.println("Adicione um valor para I (1, 2 ou 3): ");
+        int valorI = leitura.nextInt();
+
+        System.out.println("Digite o valor de A: ");
+        double valorA = leitura.nextInt();
+
+        System.out.println("Digite o valor de B: ");
+        double valorB = leitura.nextInt();
+
+        System.out.println("Digite o valor de C: ");
+        double valorC = leitura.nextInt();
+
+        if (valorI == 1) {
+
+        }
+    }
+}
